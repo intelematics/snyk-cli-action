8894343c19bc1e0aa8789478ba76ec9dbf30abf4
Nick Doyle <nick.doyle@intelematics.com>
2019-09-17 14:51:12 +1000

    feat: entrypoint write snyk run results to comment thread

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,5 +1,32 @@
 #!/bin/sh -l
+# Env vars:
+# 	SNYK_ACTION_SKIP_COMMENT	If truthy skip commenting
+#	SNYK_ACTION_WRAP_LINES		Number of lines to wrap
+
 set -eu
+
+SNYK_ACTION_WRAP_LINES_DEFAULT=10
+
+# wrap takes some output and wraps it in a collapsible markdown section if
+# it's over $SNYK_ACTION_WRAP_LINES long.
+wrap() {
+  if [[ $(echo "$1" | wc -l) -gt ${SNYK_ACTION_WRAP_LINES:-$SNYK_ACTION_WRAP_LINES_DEFAULT} ]]; then
+    echo "
+<details><summary>Show Output</summary>
+\`\`\`
+$1
+\`\`\`
+</details>
+"
+else
+    echo "
+\`\`\`
+$1
+\`\`\`
+"
+fi
+}
+
 
 # Install all the requirements
 if [ -f $2 ] ; then
@@ -11,4 +38,40 @@ else
 fi
 find $findfrom -name requirements.txt -exec bash -c 'pip install -r {}' \;
 
-sh -c "/usr/bin/snyk $*"
+# For running snyk - don't exit immediately on "failure"
+set +e
+OUTPUT=$(sh -c "snyk $*" 2>&1)
+SUCCESS=$?
+echo "$OUTPUT"
+set -e
+
+# If PR_DATA is null, then this is not a pull request event and so there's nowhere to comment
+PR_DATA=$(cat /github/workflow/event.json | jq -r .pull_request)
+if [ "$SNYK_ACTION_SKIP_COMMENT" ] || [ "$PR_DATA" = "null" ]; then
+    exit $SUCCESS
+fi
+
+
+# Build the comment we'll post to the PR
+COMMENT=""
+if [ $SUCCESS -ne 0 ]; then
+    OUTPUT=$(wrap "$OUTPUT")
+    COMMENT="#### \`SNYK\` Failed
+$OUTPUT
+*Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
+else
+    # Call wrap to optionally wrap our output in a collapsible markdown section.
+    OUTPUT=$(wrap "$OUTPUT")
+    COMMENT="#### \`SNYK\` Success
+$OUTPUT
+*Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
+fi
+
+
+# Post the comment
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+if [ "$COMMENTS_URL" != 'null' ] ; then curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null ; fi
+
+
+exit $SUCCESS
